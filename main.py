7c00eae9872eb61079b7880243ba83431988177c
Simeon <simeon.hacker@icloud.com>
2025-01-06 22:53:51 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import csv
+import requests
+
+def getDataFromWeb():
+    pass
+
+def writeDataToDB():
+    pass
+
+def createTables():
+    # 
+    genders = 'CREATE TABLE GENDERS (id int IDENTITY(1,1) PRIMARY KEY, gender varchar(255))'
+    nationalities = 'CREATE TABLE NATIONALITIES (id int IDENTITY(1,1) PRIMARY KEY, nationality varchar(255))'
+    sectors = 'CREATE TABLE SECTORS (id int IDENTITY(1,1) PRIMARY KEY, sector varchar(255))'
+    status = 'CREATE TABLE STATUS (id int IDENTITY(1,1) PRIMARY KEY, status varchar(255))'
+    dataEntries = 'CREATE TABLE dataEntries (id int IDENTITY(1,1) PRIMARY KEY, , year int, gender ,value int)'
+
+
+    queries = [genders, nationalities, sectors, status]
