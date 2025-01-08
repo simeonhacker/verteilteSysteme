3c2536eeb7aaa939a54ae901d67213780fe5fe47
Simeon <simeon.hacker@icloud.com>
2025-01-08 10:17:16 +0100

    kermit

--- main.py
+++ main.py
@@ -2,22 +2,58 @@ import csv
 import requests
 
 def getDataFromWeb():
+    # Simeon
     pass
 
 def writeDataToDB():
-    # Parse CSV File an dwrite to respective tables / Columns in DB
+    # Parse CSV File and write to respective tables / Columns in DB
+    # Select distinct from cols: gender, status, nationality, sector
+    # insert in to tables genders, statuses, nationalities, sectors
+    # select all from 
     pass
 
 def createTables():
-    # 
-    genders = 'CREATE TABLE GENDERS (id int IDENTITY(1,1) PRIMARY KEY, gender varchar(255))'
-    nationalities = 'CREATE TABLE NATIONALITIES (id int IDENTITY(1,1) PRIMARY KEY, nationality varchar(255))'
-    sectors = 'CREATE TABLE SECTORS (id int IDENTITY(1,1) PRIMARY KEY, sector varchar(255))'
-    status = 'CREATE TABLE STATUS (id int IDENTITY(1,1) PRIMARY KEY, status varchar(255))'
-    dataEntries = 'CREATE TABLE dataEntries (id int IDENTITY(1,1) PRIMARY KEY, , year int, gender ,value int)'
+    # Simeon
+    genders = '''
+        CREATE TABLE GENDERS (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            gender VARCHAR(255)
+        )
+    '''
+    nationalities = '''
+        CREATE TABLE NATIONALITIES (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            nationality VARCHAR(255)
+        )
+    '''
+    sectors = '''
+        CREATE TABLE SECTORS (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            sector VARCHAR(255)
+        )
+    '''
+    statuses = '''
+        CREATE TABLE STATUS (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            status VARCHAR(255)
+        )
+    '''
+    dataEntries = '''
+        CREATE TABLE DATA_ENTRIES (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            status INT,
+            sector INT,
+            year INT,
+            gender INT,
+            value INT,
+            FOREIGN KEY (status) REFERENCES STATUS(id),
+            FOREIGN KEY (sector) REFERENCES SECTORS(id),
+            FOREIGN KEY (gender) REFERENCES GENDERS(id)
+        )
+    '''
 
-
-    queries = [genders, nationalities, sectors, status]
+    queries = [genders, nationalities, sectors, statuses, dataEntries]
 
 def connectToDB():
+    # Louis
     pass
