d42f5302cfcffa42596fe6f966980698190eff1b
Simeon <simeon.hacker@icloud.com>
2025-01-10 20:54:09 +0100

    update 2nd grapoh

--- flaskServer.py
+++ flaskServer.py
@@ -40,6 +40,37 @@ def getDataForPlot1():
 
     return data
 
+def getDataForPlot2():
+    connection = db.connectToDB()
+    if not connection:
+        return None, None
+
+    cursor = connection.cursor()
+
+    # Query distinct years
+    cursor.execute("SELECT DISTINCT year FROM DATA_ENTRIES")
+    years = cursor.fetchall()
+    x_axes1 = [year[0] for year in years]
+
+    y_axes1 = []
+    for year in x_axes1:
+        cursor.execute("""
+            SELECT value FROM DATA_ENTRIES
+            JOIN SECTORS ON DATA_ENTRIES.sector = SECTORS.id
+            JOIN GENDERS ON DATA_ENTRIES.gender = GENDERS.id
+            JOIN NATIONALITIES ON DATA_ENTRIES.nationality = NATIONALITIES.id
+            WHERE year = %s AND SECTORS.sector = 'Total' AND GENDERS.gender = 'Frauen' AND NATIONALITIES.nationality = 'Total'
+        """, (year,))
+        values = cursor.fetchall()
+        y_axes1.extend([value[0] for value in values])
+
+    data = [x_axes1, y_axes1]
+
+    cursor.close()
+    connection.close()
+
+    return data
+
 @app.route('/plot')
 def plot():
 
@@ -47,7 +78,11 @@ def plot():
     labels = data[0]
     values = data[1]
 
-    return render_template('plot.html', labels=labels, values=values)
+    data2 = getDataForPlot2()
+    labels2 = data2[0]
+    values2 = data2[1]
+
+    return render_template('plot.html', labels=labels, values=values, labels2=labels2, values2=values2)
 
 
 
